b9c884f5a70a2bc8302d3fb8fbd8bbe900135602
Aleksandr <69845303+onik-fj@users.noreply.github.com>
2022-01-11 14:56:05 +0300

    Add files via upload

---
+++ 2_5/2_5.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let userName = "Ilya";
+
+alert( `hello ${1}` ); // выведет: hello 1
+
+alert( `hello ${"name"}` ); // выведет: hello name
+
+alert( `hello ${userName}` ); // выведет: hello Ilya
